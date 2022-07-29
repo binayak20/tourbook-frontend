96e20d06fcbcfec7d70a9714560e5a67be2e852e
Tonmoy <tonmoy@strativ.se>
2022-07-29 17:03:48 +0600

    [MW-2273] Add title to list pages which are missing

--- src/containers/Settings/Airports/SettingsAirports.tsx
+++ src/containers/Settings/Airports/SettingsAirports.tsx
@@ -1,3 +1,4 @@
+import { Typography } from '@/components/atoms';
 import { StatusColumn } from '@/components/StatusColumn';
 import { settingsAPI } from '@/libs/api';
 import { PRIVATE_ROUTES } from '@/routes/paths';
@@ -61,7 +62,12 @@ export const SettingsAirports: React.FC = () => {
 	];
 	return (
 		<div style={{ display: 'flex', height: '100%', flexDirection: 'column', gap: '1rem' }}>
-			<Row align='middle' justify='end'>
+			<Row align='middle' justify='space-between'>
+				<Col span={12}>
+					<Typography.Title level={4} type='primary' className='margin-0'>
+						{t('Airports')}
+					</Typography.Title>
+				</Col>
 				<Col>
 					<Button type='primary' size='large' onClick={() => setCreateModal(true)}>
 						{t('Create Airport')}
