31241fc21f4508252a9d4c37ad84f1bfed8559cf
Sonjoy Datta <sonjoy.dat@gmail.com>
2022-10-24 16:54:23 +0600

    added list of transctions for booking

--- src/containers/Bookings/BookingCreate/Transactions.tsx
+++ src/containers/Bookings/BookingCreate/Transactions.tsx
@@ -1,63 +1,29 @@
 import { Typography } from '@/components/atoms';
 import config from '@/config';
 import { transactionsAPI } from '@/libs/api';
-import { PRIVATE_ROUTES } from '@/routes/paths';
+import { DEFAULT_LIST_PARAMS } from '@/utils/constants';
 import { readableText } from '@/utils/helpers';
 import { Col, Row, Table } from 'antd';
 import { ColumnsType } from 'antd/lib/table';
 import moment from 'moment';
-import { useCallback, useMemo } from 'react';
+import { useMemo } from 'react';
 import { useTranslation } from 'react-i18next';
 import { useQuery } from 'react-query';
-import { Link, useNavigate, useSearchParams } from 'react-router-dom';
-import { FilterTransactions } from './FilterTransactions';
+import { useParams } from 'react-router-dom';
 
 export const Transactions = () => {
 	const { t } = useTranslation();
-	const navigate = useNavigate();
-	const [searchParams] = useSearchParams();
-	const currentPage = useMemo(() => parseInt(searchParams.get('page') || '1'), [searchParams]);
+	const { id } = useParams();
 
-	const params: API.TransactionsParams = useMemo(() => {
-		return {
-			page: currentPage,
-			name: searchParams.get('name') || undefined,
-			status: searchParams.get('status') || undefined,
-			payment_method: searchParams.get('payment_method') || undefined,
-		};
-	}, [currentPage, searchParams]);
-
-	const { data, isLoading } = useQuery(['transactions', params], () =>
-		transactionsAPI.list(params)
-	);
-
-	const handlePageChange = useCallback(
-		(page: number) => {
-			navigate(page > 1 ? `?page=${page}` : '');
-		},
-		[navigate]
+	const { data, isLoading } = useQuery(
+		['bookingTransactions'],
+		() => transactionsAPI.list({ ...DEFAULT_LIST_PARAMS, booking: id }),
+		{
+			enabled: !!id,
+		}
 	);
 
 	const columns: ColumnsType<API.Transactions> = [
-		{
-			width: 200,
-			ellipsis: true,
-			title: t('Customer'),
-			dataIndex: 'first_name',
-			render: (text, record) => `${record.first_name} ${record.last_name}`,
-		},
-		{
-			align: 'center',
-			title: t('Booking Ref.'),
-			dataIndex: 'booking_ref',
-			render: (_, record) => {
-				const bookingURL = `/dashboard/${PRIVATE_ROUTES.BOOKINGS_UPDATE.replace(
-					':id',
-					record.booking.id.toString()
-				)}`;
-				return <Link to={bookingURL}>{record.booking.reference}</Link>;
-			},
-		},
 		{
 			title: t('Date'),
 			dataIndex: 'created_at',
@@ -74,24 +40,24 @@ export const Transactions = () => {
 			),
 		},
 		{
-			title: t('Amount'),
-			dataIndex: 'amount',
-			render: (amount, record) => `${amount} ${record.currency.currency_code}`,
-		},
-		{
+			align: 'center',
 			title: t('Paid by'),
 			dataIndex: 'payment_method',
 			render: (payment_method) => <Typography.Text>{payment_method.name}</Typography.Text>,
 		},
 		{
-			align: 'center',
-			title: t('Sent to Fortnox'),
-			dataIndex: 'is_sent_to_fortnox',
-			render: (is_sent_to_fortnox) => (
-				<Typography.Text>{is_sent_to_fortnox ? t('Yes') : t('No')}</Typography.Text>
-			),
+			align: 'right',
+			title: t('Amount'),
+			dataIndex: 'amount',
+			render: (amount, record) => `${amount} ${record.currency.currency_code}`,
 		},
 	];
+
+	const totalAmount = useMemo(() => {
+		const total = data?.results.reduce((acc, cur) => acc + cur.amount, 0) || 0;
+		const currency = data?.results[0]?.currency.currency_code || '';
+		return `${parseFloat(total.toString()).toFixed(2)} ${currency}`;
+	}, [data]);
 
 	return (
 		<div style={{ display: 'flex', height: '100%', flexDirection: 'column', gap: '1rem' }}>
@@ -102,8 +68,6 @@ export const Transactions = () => {
 					</Typography.Title>
 				</Col>
 			</Row>
-
-			<FilterTransactions />
 
 			<div
 				style={{
@@ -132,7 +96,7 @@ export const Transactions = () => {
 													if (!value) return null;
 
 													return (
-														<Col span={4} key={key}>
+														<Col span={12} key={key}>
 															{readableText(key)}: {record.payment_address?.[key]}
 														</Col>
 													);
@@ -147,14 +111,18 @@ export const Transactions = () => {
 						},
 					}}
 					rowKey='id'
-					pagination={{
-						pageSize: config.itemsPerPage,
-						current: currentPage,
-						total: data?.count || 0,
-						onChange: handlePageChange,
-					}}
-					scroll={{ x: 1200, y: '100%' }}
+					pagination={false}
 					loading={isLoading}
+					footer={() => (
+						<Row justify='end'>
+							<Col>
+								<Typography.Title level={5} type='primary' className='margin-0'>
+									{t('Total')}: {totalAmount}
+								</Typography.Title>
+							</Col>
+						</Row>
+					)}
+					style={{ height: 'inherit' }}
 				/>
 			</div>
 		</div>
