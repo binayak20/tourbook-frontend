b2e7c5b6ed491c56e7f5a3b72f393add0e284477
Tonmoy <tonmoy@strativ.se>
2023-03-06 17:02:18 +0600

    [RSP-1218] Set booking v2 as default booking feature

--- src/pages/Bookings/BookingUpdate.tsx
+++ src/pages/Bookings/BookingUpdate.tsx
@@ -1,20 +1,15 @@
 import { BookingProvider } from '@/components/providers/BookingProvider';
-import { BookingUpdateContainer, BookingV2UpdateContainer } from '@/containers';
-import { useStoreSelector } from '@/store';
+import { BookingV2UpdateContainer } from '@/containers';
 import { AccessBoundary } from 'react-access-boundary';
 
 const BookingUpdate = () => {
-	const { isBetaMode } = useStoreSelector((state) => state.app);
+	// const { isBetaMode } = useStoreSelector((state) => state.app);
 
 	return (
 		<AccessBoundary to='CHANGE_BOOKING' isDefaultFallback>
-			{isBetaMode ? (
-				<BookingProvider>
-					<BookingV2UpdateContainer />
-				</BookingProvider>
-			) : (
-				<BookingUpdateContainer />
-			)}
+			<BookingProvider>
+				<BookingV2UpdateContainer />
+			</BookingProvider>
 		</AccessBoundary>
 	);
 };
