230cd65266f87bdc5f7a3ab56f7bb4d14f97e941
Tonmoy <tonmoy@strativ.se>
2023-05-19 12:51:03 +0600

    [RSP-1635] Change "is_apply" field to "is_applied" during coupon applied in booking. Both in booking create and update
    **change from "is_send_to_customer" ro "will_send_to_customer"

--- src/libs/api/bookingsAPI.ts
+++ src/libs/api/bookingsAPI.ts
@@ -97,7 +97,7 @@ class BookingsAPI extends Common {
 
 	addInvoicePayment(ID: number, is_save_and_send: boolean, payload: InvoicePaymentPayload) {
 		return this.http.post<ManualPaymentResponse>(
-			`bookings/${ID}/create-invoice-payment/?is_send_to_customer=${is_save_and_send}`,
+			`bookings/${ID}/create-invoice-payment/?will_send_to_customer=${is_save_and_send}`,
 			payload
 		);
 	}
