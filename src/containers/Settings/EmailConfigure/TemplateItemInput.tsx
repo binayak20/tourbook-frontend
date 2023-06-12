4e7725703f3ca2a4dc2ddca7f3f73b96545a0288
kaisar Sarwar <kaisar.sarwar@strativ.se>
2023-06-12 12:39:59 +0600

    edit button updated and cancel option added as undo option

--- src/containers/Settings/EmailConfigure/TemplateItemInput.tsx
+++ src/containers/Settings/EmailConfigure/TemplateItemInput.tsx
@@ -1,4 +1,4 @@
-import { EditFilled } from '@ant-design/icons';
+import { CloseCircleOutlined, EditFilled } from '@ant-design/icons';
 import { Button, Col, Form, FormItemProps, Input, Popconfirm, Row } from 'antd';
 import { FC, useEffect, useState } from 'react';
 import { useTranslation } from 'react-i18next';
@@ -9,9 +9,8 @@ export const TemplateItemInput: FC<FormItemProps> = (props) => {
 	const form = Form.useFormInstance();
 
 	const value = form.getFieldValue(`${props.name}`);
-	console.log('value', value);
 
-	useEffect(() => setDisabled(!!value), [value]);
+	useEffect(() => setDisabled(true), []);
 
 	return (
 		<Row gutter={8} align='middle'>
@@ -22,12 +21,25 @@ export const TemplateItemInput: FC<FormItemProps> = (props) => {
 			</Col>
 			<Col span={2}>
 				<Popconfirm
-					title={t('Do you really want to update?')}
-					onConfirm={() => setDisabled(!isDisabled)}
+					title={
+						isDisabled ? t('Do you really want to update?') : t('Do you really want cancel update?')
+					}
+					onConfirm={() => {
+						if (isDisabled) {
+							setDisabled(!isDisabled);
+						} else {
+							form.setFieldValue(`${props.name}`, value);
+							setDisabled(!isDisabled);
+						}
+					}}
 					okText='Yes'
 					cancelText='No'
 				>
-					<Button size='small' type='link' icon={<EditFilled />} />
+					<Button
+						size='small'
+						type='link'
+						icon={isDisabled ? <EditFilled /> : <CloseCircleOutlined />}
+					/>
 				</Popconfirm>
 			</Col>
 		</Row>
