e24bdd99014e4f0c1e3816a42e78bb279afeebb7
Tonmoy <tonmoy@strativ.se>
2023-07-05 14:39:41 +0600

    [RSP-1956, RSP-1957]Widget>Price Overview: Incorrect total price & Supplement price is being displayed on widget

--- src/widget/libs/utills.ts
+++ src/widget/libs/utills.ts
@@ -100,3 +100,20 @@ export const isPerPerson = (suppement: API.Tour['supplements'][number]) => {
 	const unitParts = suppement.unit_type?.split('_');
 	return unitParts?.includes('person');
 };
+
+export const getSupplementMultiplier = (
+	suppement?: API.Tour['supplements'][number],
+	tourDetails?: API.Tour
+) => {
+	let multiplier = 1;
+	const unitParts = suppement?.unit_type?.split('_');
+	const multiplerMap: { [key: string]: number } = {
+		week: (tourDetails?.duration || 7) / 7,
+		day: tourDetails?.duration || 1,
+		night: tourDetails?.duration || 1,
+	};
+	Object.keys(multiplerMap).forEach((key) => {
+		if (unitParts?.includes(key)) multiplier = multiplerMap[key];
+	});
+	return multiplier;
+};
