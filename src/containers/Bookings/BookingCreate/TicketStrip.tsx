059160a582cc0f05aecfb4932b0c2e485a167744
Tonmoy <tonmoy@strativ.se>
2023-09-08 10:05:52 +0600

    [RSP-2535] Update assigned ticket(ticket strip) layout in admin

--- src/containers/Bookings/BookingCreate/TicketStrip.tsx
+++ src/containers/Bookings/BookingCreate/TicketStrip.tsx
@@ -2,6 +2,7 @@ import { bookingsAPI } from '@/libs/api';
 import { AssignedTicket } from '@/libs/api/@types';
 import { DeleteOutlined } from '@ant-design/icons';
 import { Button, message, Popconfirm } from 'antd';
+import moment from 'moment';
 import { FC } from 'react';
 import { useAccessContext } from 'react-access-boundary';
 import { useTranslation } from 'react-i18next';
@@ -32,28 +33,29 @@ const TicketStrip: FC<{
 						<div className='label'>{t('PNR')}</div>
 						<div className='value'>{item.ticket.pnr}</div>
 					</div>
-					<div className='group'>
-						<div className='label parent'>{t('Outbound')}</div>
-						<div className='label child cl1'>{t('Date')}</div>
-						<div className='value cv1'>{item.ticket.ticket_outbound_date}</div>
-						<div className='label child cl2'>{t('Time')}</div>
-						<div className='value cv2'>{item.ticket.outbound_departure_time}</div>
-						<div className='label child cl3'>{t('Location')}</div>
-						<div className='value cv3'>{item.ticket.departure_station?.name}</div>
-						<div className='label child cl4'>{t('Flight')}</div>
-						<div className='value cv4'>{item.ticket.outbound_flight_no}</div>
+					<div className='mid-section'>
+						<div className='ticket'>
+							<div>{moment(item.ticket.ticket_outbound_date).format('DD MMM YYYY')}</div>
+							<div>{item.ticket.outbound_flight_no}</div>
+							<div>
+								{item.ticket.departure_station?.name} - {item.ticket.destination_station?.name}
+							</div>
+							<div>
+								{item.ticket.outbound_departure_time} - {item.ticket.outbound_arrival_time}
+							</div>
+						</div>
+						<div className='ticket'>
+							<div>{moment(item.ticket.ticket_inbound_date).format('DD MMM YYYY')}</div>
+							<div>{item.ticket.inbound_flight_no}</div>
+							<div>
+								{item.ticket.destination_station?.name} - {item.ticket.departure_station?.name}
+							</div>
+							<div>
+								{item.ticket.inbound_departure_time} - {item.ticket.inbound_arrival_time}
+							</div>
+						</div>
 					</div>
-					<div className='group'>
-						<div className='label parent'>{t('Inbound')}</div>
-						<div className='label child cl1'>{t('Date')}</div>
-						<div className='value cv1'>{item.ticket.ticket_inbound_date}</div>
-						<div className='label child cl2'>{t('Time')}</div>
-						<div className='value cv2'>{item.ticket.inbound_departure_time}</div>
-						<div className='label child cl3'>{t('Location')}</div>
-						<div className='value cv3'>{item.ticket.destination_station?.name}</div>
-						<div className='label child cl4'>{t('Flight')}</div>
-						<div className='value cv4'>{item.ticket.inbound_flight_no}</div>
-					</div>
+
 					<div className='solo text-center'>
 						<div className='label'>{t('Supplier')}</div>
 						<div className='value'>{item.ticket.ticket_supplier?.name}</div>
@@ -86,62 +88,21 @@ const Strip = styled.div`
 	padding: 0.75rem;
 	margin-bottom: 0.75rem;
 	border: 1px dashed var(--ant-primary-4);
-	grid-template-columns: auto auto auto auto;
-	.label {
-		opacity: 0.65;
-		padding: 0.25rem 0;
-		&.cl1 {
-			grid-area: cl1;
-		}
-		&.cl2 {
-			grid-area: cl2;
-		}
-		&.cl3 {
-			grid-area: cl3;
-		}
-		&.cl4 {
-			grid-area: cl4;
-		}
-	}
+	grid-template-columns: 1fr auto 1fr;
 	.value {
 		color: var(--ant-primary-color);
 		font-weight: 600;
 		padding: 0.25rem 0;
-		&.cv1 {
-			grid-area: cv1;
-		}
-		&.cv2 {
-			grid-area: cv2;
-		}
-		&.cv3 {
-			grid-area: cv3;
-		}
-		&.cv4 {
-			grid-area: cv4;
-		}
 	}
+	.label {
+		opacity: 0.65;
+		padding: 0.25rem 0;
+	}
+
 	.solo {
 		display: flex;
 		flex-direction: column;
 		justify-content: center;
-	}
-	.group {
-		display: grid;
-		grid-template-areas:
-			'p p p p'
-			'cl1 cl2 cl3 cl4'
-			'cv1 cv2 cv3 cv4';
-		grid-template-columns: repeat(4, auto);
-		grid-template-rows: 1fr 1fr 1fr;
-	}
-	.parent {
-		grid-area: p;
-		width: 100%;
-		text-align: center;
-	}
-	.child {
-		border-top: 1px dashed rgba(0, 0, 0, 0.4);
-		border-bottom: 1px dashed rgba(0, 0, 0, 0.4);
 	}
 	.text-center {
 		text-align: center;
@@ -150,6 +111,19 @@ const Strip = styled.div`
 		position: absolute;
 		right: 0.25rem;
 		top: 0.25rem;
+	}
+	.mid-section {
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		gap: 0.5rem;
+		margin-left: -3rem;
+		.ticket {
+			display: flex;
+			gap: 1.5rem;
+			color: var(--ant-primary-color);
+			font-weight: 500;
+		}
 	}
 `;
 
